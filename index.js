b34bfc15cf1d97d9e42075beb7b491fb4dd9443e
Ben Janes <brjanes@gmail.com>
2016-04-04 17:08:25 -0400

    add socket.io to dependencies, start writing server code

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 8080;
+
+var allRooms = [];
+var allClients = [];
 
 app.use(express.static('public'));
 
