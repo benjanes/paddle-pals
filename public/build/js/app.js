79f18b15381433016252cb561df27bdf97e96c13
Ben Janes <brjanes@gmail.com>
2016-04-04 16:09:11 -0400

    add game board

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1,9 +1,23 @@
 var paddleApp = (function() {
   var app = {};
 
+  // game variables
+  var width = 500;
+  var height = 500;
+
+  /****************
+  ** The Game Board
+  ****************/
+  function setupBoard() {
+    board = d3.select('#game_board')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+  }
+
   app.init = function() {
-    console.log('start app');
+    setupBoard();
   };
-  
+
   return app;
 })();
