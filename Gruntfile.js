4f54689bc78c4770b40147171dbf21b29ac6d48a
Ben Janes <brjanes@gmail.com>
2016-04-04 15:02:28 -0400

    add gruntfile, build process

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  var jsLibs = ['bower_components/d3/d3.min.js'];
+  var jsApp = ['public/build/js/app.js'];
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'public/dist/styles/style.css' : 'public/build/sass/style.sass'
+        }
+      }
+    },
+
+    autoprefixer: {
+      dist: {
+        files: {
+          'public/dist/styles/style.css' : 'public/dist/styles/style.css'
+        }
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js', 'public/build/js/**/*.js', 'index.js']
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'public/dist/js/libs.min.js': [jsLibs],
+          'public/dist/js/app.min.js': [jsApp],
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: 'public/build/sass/*.sass',
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      },
+      jshint: {
+        files: ['public/build/js/**/*.js'],
+        tasks: ['jshint']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+
+  grunt.registerTask('default', ['sass', 'jshint', 'watch']);
+  grunt.registerTask('build', ['sass', 'autoprefixer', 'uglify']);
+}
